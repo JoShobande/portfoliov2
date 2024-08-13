8dfc4640b1f81295d4122e1f1f679d89af4e7577
Josephine <shobande.josephin@gmail.com>
2024-08-12 21:00:57 -0400

    implemented nav bar for desktop and mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,53 @@
+'use client'
+
 import Image from "next/image";
-import Link from "next/link"
-import clsx from 'clsx';
+import { useState } from "react";
+import styles from './componentStyles.module.scss'
 
 const Header = () => {
+
+    const [openMobileMenu, setOpenMobileMenu] = useState(false)
+
     return(
         <header>
-            <nav className='flex items-center justify-between w-[80%] m-auto'>
-                <Image 
-                    src={'/roundedLogo.png'}
-                    alt='jossy_logo'
-                    width={120}
-                    height={120}
-                /> 
-                <ul className='flex justify-around'>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Services</li>
-                    <li>Projects</li>
-                    <li>My Blog</li>
-                    <li>Contact</li>
-                </ul>
+            <nav className={`${openMobileMenu ? '': 'items-center' } flex md:items-center justify-between lg:w-[80%] m-auto`}>
+                <div className='w-[100px] h-[100px] lg:w-[150px] lg:h-[150px] relative'>
+                    <Image 
+                        src={'/roundedLogo.png'}
+                        alt='jossy_logo'
+                        fill={true}
+                        objectFit="contain" 
+                        className='cursor-pointer'
+                    /> 
+                </div>
+
+                <div 
+                    className={`${openMobileMenu ? 'hidden' : 'block' } md:hidden pr-[10px]`}
+                    onClick={()=>setOpenMobileMenu(true)}
+                >
+                    <div className='w-[30px] h-[5px] rounded-[8px] bg-[red] mb-[1px]'/>
+                    <div className='w-[30px] h-[5px] rounded-[8px] bg-[red] mb-[1px]'/>
+                    <div className='w-[30px] h-[5px] rounded-[8px] bg-[red] mb-[1px]'/>
+                </div>
+
+                <div className={`${openMobileMenu ? styles['menu-open']: styles['menu']}`}>
+                    <div 
+                        className='flex md:hidden flex-col items-end'
+                        onClick={()=>setOpenMobileMenu(false)}
+                    >
+                        <p className='text-[24px] mb-[20px]'>X</p>
+                    </div>
+                    <ul className='md:flex justify-around gap-x-[20px]' >
+                        <li className='text-[18px]'>Home</li>
+                        <li className='text-[18px]'>About</li>
+                        <li className='text-[18px]'>Experience</li>
+                        <li className='text-[18px]'>Services</li>
+                        <li className='text-[18px]'>Projects</li>
+                        <li className='text-[18px]'>Contact</li>
+                    </ul>
+                </div>
+                
+               
             </nav>
         </header>
     )
