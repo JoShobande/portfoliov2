3b831c869f3f95a1021c1720a4f04079c3fac6ec
Josephine <shobande.josephin@gmail.com>
2024-08-14 21:41:16 -0400

    added hero bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@components/components/Hero";
 
 
 export default function Home() {
   return (
     <main className="">
-      
+       <Hero/>
     </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Image from "next/image";
+import { useState } from "react";
+import styles from './componentStyles.module.scss'
+
+const Hero = () => {
+
+  
+
+    return(
+        <section className={`bg-hero-bg h-[100vh] bg-cover bg-center `}>
+            
+        </section>
+    )
+}
+
+export default Hero;
